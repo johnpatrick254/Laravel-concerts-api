d83dc0476134e3e83485a330c1563f5e073bc217
johnpatrick254 <jpattrick538@gmail.com>
2024-03-27 19:01:57 +0300

    feat:create db seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::factory(1000)->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call(EventSeeder::class);
+        $this->call(AttendeeSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
+namespace App\http\Controllers;
 
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\EventsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::apiResource('events',EventsController::class);
+Route::apiResource('events.attendee',AttendeeController::class)->scoped(['attendee'=>'event']);
